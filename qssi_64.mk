8016235fb7bcad9bed1d6f4ecc7404b782513ecf
renjiezhang <renjiezhang@t2mobile.com>
2024-11-30 22:29:20 +0800

    FPS-252 [FPS][Euicc]esim not work[3/3]
    * Root Cause
      Customer's requestment
    * Solution
      Fix code to meet customer' requestment
    * Test Steps
      Please refer to this Jira's Description -> Reproduce steps.
    
    Change-Id: I64854360d05d6ce6aa4a9861897b607c41dc12aa

--- qssi_64.mk
+++ qssi_64.mk
@@ -254,12 +254,20 @@ PRODUCT_PACKAGES += \
     vendor.qti.power.pasrmanager@1.0-impl \
     pasrservice
 
+PRODUCT_COPY_FILES += \
+frameworks/native/data/etc/android.hardware.telephony.euicc.xml:system/etc/permissions/android.hardware.telephony.euicc.xml
+
+PRODUCT_PACKAGES += EuiccGoogle FPEuiccOverlay
+
 #Enable sysrilcmd
 PRODUCT_PACKAGES += \
                     qcrilhook \
                     qcrilhook.xml \
                     sysrilcmd \
                     sysrilcmd.xml
+
+PRODUCT_PACKAGES += EuiccOverlay
+PRODUCT_PACKAGES += SetupWizardOverlay
 
 # Kernel modules install path
 KERNEL_MODULES_INSTALL := dlkm
@@ -337,6 +345,8 @@ $(call inherit-product, packages/modules/Virtualization/build/apex/product_packa
 
 #CustomerCarrierConfig
 $(call inherit-product-if-exists, vendor/fairphone/source/apps/CustomerCarrierConfig/Customer_Carrier.mk)
+
+$(call inherit-product-if-exists, vendor/fairphone/source/apps/customer_carrier/customer_carrier.mk)
 
 ###################################################################################
 # This is the End of target.mk file.
