a017c5e5615275398b47ed3c7d2cf8ca6042e392
Aakruti Saini <aakruti.saini@siliconsignals.io>
2024-12-20 14:30:18 +0530

    FP6: Inherit Makefiles for proprietary project
    Issue: FP6-A16#12
    Change-Id: I7224bc38f005c7116628db0ed777bf1c95962dbb

--- qssi_64.mk
+++ qssi_64.mk
@@ -464,3 +464,6 @@ $(call inherit-product-if-exists, vendor/fairphone/apps/fps-apps.mk)
 #+ FPS-53. Disable Android's rescue party. xuguang.yang. 20250218
 PRODUCT_SYSTEM_EXT_PROPERTIES += persist.sys.disable_rescue=true
 #- FPS-53. Disable Android's rescue party. xuguang.yang. 20230218
+
+# Inherit the proprietary setup
+$(call inherit-product, device/fairphone/fp6-proprietary/device-vendor.mk)
