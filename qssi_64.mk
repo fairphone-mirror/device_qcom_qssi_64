7532fe410a24e26749afa45f92b82459c02234bc
xuguang.yang <xuguang.yang@t2mobile.com>
2025-02-20 10:14:43 +0800

    [FPS-628][1/2]RKP implement
    * Comment: RKP implement
    * Root Cause: Requirement
    * Solution: Integrated tools rkp_factory_extraction_tool64 
          and default set vendor.wv.oemcrypto.debug.enable_prov40 to true for mini building
    * Test Steps: Collecting CSR information by rkp_factory_extraction_tool64 on secure device
          adb shell rkp_factory_extraction_tool64 --output_format build+csr > csr.json
    * Test Result: pass
    
    Change-Id: I46618f001da080d390708565527ee7c4edc273c4

--- qssi_64.mk
+++ qssi_64.mk
@@ -277,6 +277,10 @@ endif
 
 ifeq ($(TARGET_BUILD_MMITEST),true)
 PRODUCT_PACKAGES += Dialer
+#+FPS-628 add by xuguang.yang for RKP
+PRODUCT_PACKAGES += rkp_factory_extraction_tool
+PRODUCT_SYSTEM_EXT_PROPERTIES += vendor.wv.oemcrypto.debug.enable_prov40=true
+#-FPS-628
 endif
 
 #MMITest
