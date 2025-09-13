569f40d2f976a026080f9d0f107ba322e4a3e606
liquan.zhou <liquan.zhou@t2mobile.com>
2025-09-13 15:09:07 +0800

    [FPS-3327] GtsKeyMintAttestationTestCases:com.google.android.gts.keymintattestation.NoBatchAttestationTest#testGoogleCertifiedFactoryProvisionedKeyExistsTee failed
    * Comment: The Googke keybox is using RKP only, so need to set remote_provisioning.tee.rkp_only as true
    * Solution: Add remote_provisioning.tee.rkp_only=true in property
    * Root Cause: GTS bug
    * Test Steps: run gts-root -m GtsKeyMintAttestationTestCases
    * Test Result: Pass
    
    Change-Id: I8a87560d210a90529c1ab7f3589bf65edce4d5bb

--- qssi_64.mk
+++ qssi_64.mk
@@ -98,6 +98,7 @@ endif
 PRODUCT_PRODUCT_PROPERTIES += \
     remote_provisioning.enable_rkpd=true \
     remote_provisioning.hostname=remoteprovisioning.googleapis.com \
+    remote_provisioning.tee.rkp_only=true
 
 PRODUCT_SOONG_NAMESPACES += \
     hardware/google/av \
